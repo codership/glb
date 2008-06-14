6b44e3f1e71fbaf9baef2d043dce38b3818017c8
Alexey Yurchenko <alexey.yurchenko@codership.com>
2008-06-14 14:30:50 +0000

    Intermediate commit

---
+++ src/glb_pool.c
@@ -0,0 +1,8 @@
+/*
+ * Copyright (C) 2008 Codership Oy <info@codership.com>
+ *
+ * $Id$
+ */
+
+#include "glb_pool.h"
+
---
+++ src/glb_pool.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2008 Codership Oy <info@codership.com>
+ *
+ * $Id$
+ */
+
+#ifndef _glb_router_h_
+#define _glb_router_h_
+
+
+#endif // _glb_router_h_
