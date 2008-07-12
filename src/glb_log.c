14faf6c833fa90f60230972fe49e554325edceea
Alexey Yurchenko <alexey.yurchenko@codership.com>
2008-07-12 15:49:48 +0000

    Added logging module. Empty so far.

---
+++ src/glb_log.c
@@ -0,0 +1,10 @@
+/*
+ * Copyright (C) 2008 Codership Oy <info@codership.com>
+ *
+ * $Id$
+ */
+
+#include <syslog.h>
+
+#include "glb_log.h"
+
---
+++ src/glb_log.h
@@ -0,0 +1,10 @@
+/*
+ * Copyright (C) 2008 Codership Oy <info@codership.com>
+ *
+ * $Id$
+ */
+
+#ifndef _glb_log_h_
+#define _glb_log_h_
+
+#endif // _glb_log_h_
